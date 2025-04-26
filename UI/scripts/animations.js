b99bc70f811352ba9e8e8042079250acb1ac4499
dragospy <dragosesoalca@gmail.com>
2025-04-26 12:50:29 +0300

    Uploaded UI changes to new branch

---
+++ UI/scripts/animations.js
@@ -0,0 +1,28 @@
+//Card Button click animation
+export function rotateAnim(element, degreesIncrement) {
+    let degrees = parseInt(element.css("--degrees")) || 0;
+    
+    if (degrees >= 360){
+        element.css({
+            '--degrees' : `${0}`
+        });
+
+        degrees = 0;
+    } // Resets button --degrees var to 0 once it reaches 360
+
+    degrees += degreesIncrement;
+
+    element.animate(
+        { deg: degrees },
+        {
+            duration: 600,
+            step: function (now) {
+                $(this).css({ transform: 'rotate(' + now + 'deg)' });
+            }
+        }
+    );
+
+    element.css({
+        '--degrees': `${degrees}`
+    });
+}
